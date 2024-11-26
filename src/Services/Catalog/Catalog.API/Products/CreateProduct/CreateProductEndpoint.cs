139e5b1782e3d296fbf7e05edbbf45ce95deb248
Saidul Islam Rajib <saidulislamrajib1997@gmail.com>
2024-11-26 22:12:46 +0600

    Add new package named `Carter`

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,5 +1,14 @@
 ﻿namespace Catalog.API.Products.CreateProduct
 {
+    public record CreateProductRequest(
+        string Name,
+        List<string> Cateogry,
+        string Description,
+        string ImageFile,
+        decimal Price);
+
+    public record CreateProductResponse(Guid Id);
+
     public class CreateProductEndpoint
     {
     }
