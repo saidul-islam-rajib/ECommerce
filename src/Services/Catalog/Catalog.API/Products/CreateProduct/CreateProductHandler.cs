42a6c1bcb0f681a7b5afcc76f51ac77d4d5458c0
Saidul Islam Rajib <saidulislamrajib1997@gmail.com>
2024-11-26 21:12:33 +0600

    Created `Catalog.API` project

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    // represent the data that we need to create
+    public record CreateProductCommand(
+        string Name,
+        List<string> Cateogry,
+        string Description,
+        string ImageFile,
+        decimal Price) : IRequest<CreateProductResult>;
+
+    public record CreateProductResult(Guid Id);
+    internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    {
+        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            // perform business logic to create a new product
+
+
+            throw new NotImplementedException();
+        }
+    }
+}
