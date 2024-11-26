4248d51499331066e4ede805928232bbb39f19b0
Saidul Islam Rajib <saidulislamrajib1997@gmail.com>
2024-11-27 00:18:33 +0600

    Add connection string

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -9,6 +9,11 @@ builder.Services.AddMediatR(config =>
     config.RegisterServicesFromAssembly(typeof(Program).Assembly);
 });
 
+builder.Services.AddMarten(options =>
+{
+    options.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
+
 
 var app = builder.Build();
 
