61c33c60144c887feae5d955a21d86c779b6c9a4
Saidul Islam Rajib <saidulislamrajib1997@gmail.com>
2024-11-27 22:27:13 +0600

    Get all products

---
+++ src/Services/Catalog/Catalog.API/Products/GetProduct/GetProductEndpoint.cs
@@ -0,0 +1,25 @@
+﻿using Catalog.API.Models;
+
+namespace Catalog.API.Products.GetProduct
+{
+    // public record GetProductsRequest();
+
+    public record GetProductsResponse(IEnumerable<Product> Products);
+
+    public class GetProductEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products", async (ISender sender) =>
+            {
+                var result = await sender.Send(new GetProductsQuery());
+                var response = result.Adapt<GetProductsResponse>();
+
+                return Results.Ok(response);
+            }).WithName("GetProducts")
+                .Produces<GetProductsResponse>(StatusCodes.Status201Created)
+                .WithSummary("Get Products")
+                .WithDescription("Getting all products are successfully completed.");
+        }
+    }
+}
