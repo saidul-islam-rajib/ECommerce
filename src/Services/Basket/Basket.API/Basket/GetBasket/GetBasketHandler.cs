4a1302f33c39a6e9992da08af52f77618af9272e
Saidul Islam Rajib <saidulislamrajib1997@gmail.com>
2024-12-17 21:07:25 +0600

    modify handler class in basket microservice

--- src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -1,6 +1,14 @@
 ﻿namespace Basket.API.Basket.GetBasket
 {
-    public class GetBasketHandler
+    public record GetBasketQuery(string UserName) : IQuery<GetBasketResult>;
+    public record GetBasketResult(ShoppingCart Cart);
+    public class GetBasketQueryHandler : IQueryHandler<GetBasketQuery, GetBasketResult>
     {
+        public async Task<GetBasketResult> Handle(GetBasketQuery query, CancellationToken cancellationToken)
+        {
+            // TODO: get basket from database
+
+            return new GetBasketResult(new ShoppingCart("rajib"));
+        }
     }
 }
