c451dcc2896ba6ebb94cddbd85b631a43da8e002
Saidul Islam Rajib <saidulislamrajib1997@gmail.com>
2024-12-17 21:18:38 +0600

    Develop get basket endpoint with minimal apis

--- src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoints.cs
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoints.cs
@@ -1,6 +1,25 @@
 ﻿namespace Basket.API.Basket.GetBasket
 {
-    public class GetBasketEndpoints
+    // public record GetBasketRequest(string UserName);
+
+    public record GetBasketResponse(ShoppingCart Cart);
+
+
+    public class GetBasketEndpoints : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/basket/{userName}", async (string userName, ISender sender) =>
+            {
+                var result = await sender.Send(new GetBasketQuery(userName));
+                var response = result.Adapt<GetBasketResponse>();
+
+                return Results.Ok(response);
+            })
+            .WithName("GetAllBaskets")
+            .Produces<GetBasketResponse>(StatusCodes.Status201Created)
+            .WithSummary("Get Basket")
+            .WithDescription("Get baskets");
+    }
     }
 }
