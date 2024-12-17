209f69f7dfd226fe83aa6311a02a408bf49dea63
Saidul Islam Rajib <saidulislamrajib1997@gmail.com>
2024-12-18 00:35:03 +0600

    Integrated `Redis` cache for `Basket` microservice

---
+++ src/Services/Basket/Basket.API/Data/CachedBasketRepository.cs
@@ -0,0 +1,37 @@
+﻿
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace Basket.API.Data
+{
+    // Proxy pattern: here `CachedBasketRepository` as as proxy
+    // Decorator pattern: here `BasketRepository` extends by adding new `CachedBasketRepository`
+    public class CachedBasketRepository(IBasketRepository repository, IDistributedCache cache ) : IBasketRepository
+    {
+        public async Task<ShoppingCart> GetBasket(string userName, CancellationToken cancellationToken = default)
+        {
+            var cachedBasket = await cache.GetStringAsync(userName, cancellationToken);
+            if (!string.IsNullOrEmpty(cachedBasket)){
+                return JsonSerializer.Deserialize<ShoppingCart>(cachedBasket)!;
+            }
+            var basket = await repository.GetBasket(userName, cancellationToken);
+            await cache.SetStringAsync(userName, JsonSerializer.Serialize(basket), cancellationToken);
+
+            return basket;
+        }
+
+        public async Task<ShoppingCart> StoreBasket(ShoppingCart basket, CancellationToken cancellationToken = default)
+        {
+            await repository.StoreBasket(basket, cancellationToken);
+            await cache.SetStringAsync(basket.UserName, JsonSerializer.Serialize(basket), cancellationToken);
+            return basket;
+        }
+
+        public async Task<bool> DeleteBasket(string username, CancellationToken cancellationToken = default)
+        {
+            await repository.DeleteBasket(username, cancellationToken);
+            await cache.RemoveAsync(username, cancellationToken);
+            return true;
+        }
+    }
+}
