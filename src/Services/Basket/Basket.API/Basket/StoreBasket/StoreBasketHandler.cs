7309b2a1923e5830c1cdf2f5dfb4c9c32549005c
Saidul Islam Rajib <saidulislamrajib1997@gmail.com>
2024-12-17 21:27:57 +0600

    develop store basket feature handler with minimal apis for `store basket`

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,6 +1,27 @@
 ﻿namespace Basket.API.Basket.StoreBasket
 {
-    public class StoreBasketHandler
+    public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
+    public record StoreBasketResult(string UserName);
+
+    public class StoreBasketCommandValidation : AbstractValidator<StoreBasketCommand>
     {
+        public StoreBasketCommandValidation()
+        {
+            RuleFor(x => x.Cart).NotNull().WithMessage("Cart can not be null");
+            RuleFor(x => x.Cart.UserName).NotNull().WithMessage("Username is required");
+        }
+    }
+    public class StoreBasketCommandHandler : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+    {
+        public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
+        {
+            ShoppingCart cart = command.Cart;
+
+            // TODO: store the basket in DB
+            // TODO: update the cache
+
+            return new StoreBasketResult("rajib");
+
+        }
     }
 }
