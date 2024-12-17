ff3ea6f3dc9c4551741a090b37f08d55e226e26f
Saidul Islam Rajib <saidulislamrajib1997@gmail.com>
2024-12-17 21:35:32 +0600

    Developed `Endpoints` to store basket

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketEndpoints.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketEndpoints.cs
@@ -1,6 +1,32 @@
-﻿namespace Basket.API.Basket.StoreBasket
+﻿using Basket.API.Basket.GetBasket;
+
+namespace Basket.API.Basket.StoreBasket
 {
-    public class StoreBasketEndpoints
+    public record StoreBasketRequest(ShoppingCart Cart);
+    public record StoreBasketResponse(string UserName);
+    public class StoreBasketEndpoints : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/basket/", async (StoreBasketRequest request, ISender sender) =>
+            {
+                // 1. Adapt incoming `request` to `command` object
+                var command = request.Adapt<StoreBasketCommand>();
+
+                // 2. Send this `command` object through `mediatR`
+                var result = await sender.Send(command);
+
+                // 3. Adapt this `result` object to `response` object
+                var response = result.Adapt<StoreBasketResponse>();
+
+                // 4. Return to the client as a Result
+                return Results.Created($"/basket/{response.UserName}", response);
+
+            })
+            .WithName("Create Basket")
+            .Produces<StoreBasketResponse>(StatusCodes.Status201Created)
+            .WithSummary("Create Basket")
+            .WithDescription("Create Basket");
+        }
     }
 }
