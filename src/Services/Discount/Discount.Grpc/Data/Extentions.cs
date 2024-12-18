cd971f951bc166331da9ea197c573a5d890efee6
Saidul Islam Rajib <saidulislamrajib1997@gmail.com>
2024-12-18 23:28:26 +0600

    Add and register migration extensions method

---
+++ src/Services/Discount/Discount.Grpc/Data/Extentions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data
+{
+    public static class Extentions
+    {
+        public static IApplicationBuilder UseMigration(this IApplicationBuilder app)
+        {
+            using var scope = app.ApplicationServices.CreateScope(); // helps to get db context obj
+            using var dbContext = scope.ServiceProvider.GetRequiredService<DiscountContext>(); // reach db context obj
+
+            // now perform auto migration
+            dbContext.Database.MigrateAsync();
+
+            return app;
+        }
+    }
+}
