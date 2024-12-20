0953baaba7010c721b89f02043665b4148421346
Saidul Islam Rajib <saidulislamrajib1997@gmail.com>
2024-12-20 11:43:08 +0600

    Developed domain modeling with DDD for `Ordering` microservice

---
+++ src/Services/Ordering/Ordering.Domain/Models/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace Ordering.Domain.Models
+{
+    public class Product : Entity<Guid>
+    {
+        public string ProductName { get; private set; } = default!;
+        public string Price { get; private set; } = default!;
+    }
+}
