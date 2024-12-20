8229a91fb6e9f44aeb67f7b3c1cad59e7caa1493
Saidul Islam Rajib <saidulislamrajib1997@gmail.com>
2024-12-20 11:57:33 +0600

    Developed stringly typed ID's for `Order` aggregate

--- src/Services/Ordering/Ordering.Domain/Models/Product.cs
+++ src/Services/Ordering/Ordering.Domain/Models/Product.cs
@@ -1,6 +1,6 @@
 ﻿namespace Ordering.Domain.Models
 {
-    public class Product : Entity<Guid>
+    public class Product : Entity<ProductId>
     {
         public string ProductName { get; private set; } = default!;
         public string Price { get; private set; } = default!;
