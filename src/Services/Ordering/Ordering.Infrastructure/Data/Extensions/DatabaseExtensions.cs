9bd82832dfe635afa22ceeeaf424ea23d99b3550
Saidul Islam Rajib <saidulislamrajib1997@gmail.com>
2024-12-20 18:08:02 +0600

    Seed data

--- src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/DatabaseExtensions.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/DatabaseExtensions.cs
@@ -11,6 +11,41 @@ namespace Ordering.Infrastructure.Data.Extensions
             var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
 
             context.Database.MigrateAsync().GetAwaiter().GetResult();
+
+            await SeedAsync(context);
+        }
+
+        private static async Task SeedAsync(ApplicationDbContext context)
+        {
+            // Sedding tables one by one
+            await SeedCustomerAsync(context);
+            await SeedProductAsync(context);
+            await SeedOrderedItemAsync(context);
+        }
+
+        private static async Task SeedCustomerAsync(ApplicationDbContext context)
+        {
+            if(!await context.Customers.AnyAsync())
+            {
+                await context.Customers.AddRangeAsync(InitialData.Customers);
+                await context.SaveChangesAsync();
+            }
+        }
+        private static async Task SeedProductAsync(ApplicationDbContext context)
+        {
+            if (!await context.Products.AnyAsync())
+            {
+                await context.Products.AddRangeAsync(InitialData.Products);
+                await context.SaveChangesAsync();
+            }
+        }
+        private static async Task SeedOrderedItemAsync(ApplicationDbContext context)
+        {
+            if (!await context.Orders.AnyAsync())
+            {
+                await context.Orders.AddRangeAsync(InitialData.OrdersWithItems);
+                await context.SaveChangesAsync();
+            }
         }
     }
 }
