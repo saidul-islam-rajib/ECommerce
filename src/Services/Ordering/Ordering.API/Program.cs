d990746dd522771c0d8f1d320b7aa5eb3f3dce2d
Saidul Islam Rajib <saidulislamrajib1997@gmail.com>
2024-12-20 17:14:42 +0600

    Auto migrate EF core entities to SQL server when application startup

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,6 +1,7 @@
 using Ordering.API;
 using Ordering.Application;
 using Ordering.Infrastructure;
+using Ordering.Infrastructure.Data.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +15,12 @@ var app = builder.Build();
 
 
 // ............ CONFIGURE HTTP REQUEST PIPELINE ............
+app.UseApiServices();
 
+
+if (app.Environment.IsDevelopment())
+{
+    await app.InitialiseDatabaseAsync();
+}
 
 app.Run();
