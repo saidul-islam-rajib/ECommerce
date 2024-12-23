ded5aa1bb140c977af347fcc352e6210f8a4321e
Saidul Islam Rajib <saidulislamrajib1997@gmail.com>
2024-12-24 00:40:20 +0600

    Develop `DeleteOrderCommand` for Ordering.Application

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/DeleteOrder/DeleteOrderHandler.cs
@@ -0,0 +1,24 @@
+﻿namespace Ordering.Application.Orders.Commands.DeleteOrder
+{
+    public class DeleteOrderHandler(IApplicationDbContext dbContext)
+        : ICommandHandler<DeleteOrderCommand, DeleteOrderResult>
+    {
+        public async Task<DeleteOrderResult> Handle(DeleteOrderCommand command, CancellationToken cancellationToken)
+        {
+            // 1. Delete order entity from command object
+            var orderId = OrderId.Of(command.OrderId);
+            var order = await dbContext.Orders.FindAsync([orderId], cancellationToken: cancellationToken);
+            if(order is null)
+            {
+                throw new OrderNotFoundException(command.OrderId);
+            }
+
+            // 2. Save to database
+            dbContext.Orders.Remove(order);
+            await dbContext.SaveChangesAsync(cancellationToken);
+
+            // 3. Return result
+            return new DeleteOrderResult(true);
+        }
+    }
+}
