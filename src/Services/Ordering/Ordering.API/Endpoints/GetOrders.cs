edf764929df4338d243d7b05df67e72d24b67098
Saidul Islam Rajib <saidulislamrajib1997@gmail.com>
2024-12-25 01:11:48 +0600

    result will adapt into response

--- src/Services/Ordering/Ordering.API/Endpoints/GetOrders.cs
+++ src/Services/Ordering/Ordering.API/Endpoints/GetOrders.cs
@@ -12,7 +12,7 @@ public class GetOrders : ICarterModule
         app.MapGet("/orders", async ([AsParameters] PaginationRequest request, ISender sender) =>
         {
             var result = await sender.Send(new GetOrdersQuery(request));
-            var response = request.Adapt<GetOrdersResponse>();
+            var response = result.Adapt<GetOrdersResponse>();
 
             return Results.Ok(response);
         })
