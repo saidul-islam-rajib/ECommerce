b6baa4388ca1a3c38eb4d2ce3f3605f9d4b022b7
Saidul Islam Rajib <saidulislamrajib1997@gmail.com>
2024-12-24 19:25:51 +0600

    Develop extension method for `Project to order` dto

--- src/Services/Ordering/Ordering.Application/Dtos/OrderItemDto.cs
+++ src/Services/Ordering/Ordering.Application/Dtos/OrderItemDto.cs
@@ -1,4 +1,8 @@
 ﻿namespace Ordering.Application.Dtos;
 
 
-public record OrderItemDto(Guid OrderId, Guid ProductId, int Quantity, decimal Price);
+public record OrderItemDto(
+    Guid OrderId,
+    Guid ProductId,
+    int Quantity,
+    decimal Price);
--- src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByName/GetOrdersByNameHandler.cs
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByName/GetOrdersByNameHandler.cs
@@ -1,4 +1,6 @@
-﻿namespace Ordering.Application.Orders.Queries.GetOrdersByName
+﻿using Ordering.Application.Extensions;
+
+namespace Ordering.Application.Orders.Queries.GetOrdersByName
 {
     public class GetOrdersByNameHandler(IApplicationDbContext dbContext)
         : IQueryHandler<GetOrdersByName, GetOrdersByNameResult>
@@ -12,10 +14,9 @@
                 .OrderBy(o => o.OrderName)
                 .ToListAsync();
 
-            var orderDtos = ProjectToOrderDto(orders);
-
+            
             // 2. Return result
-            return new GetOrdersByNameResult(orderDtos);
+            return new GetOrdersByNameResult(orders.ToOrderDtoList());
         }
 
         private IEnumerable<OrderDto> ProjectToOrderDto(List<Order> orders)
@@ -26,7 +27,7 @@
                 var shippingAddress = new AddressDto(
                     order.ShippingAddress.FirstName,
                     order.ShippingAddress.LastName,
-                    order.ShippingAddress.EmailAddress,
+                    order.ShippingAddress.EmailAddress!,
                     order.ShippingAddress.AddressLine,
                     order.ShippingAddress.Country,
                     order.ShippingAddress.State,
@@ -35,7 +36,7 @@
                 var billingAddress = new AddressDto(
                     order.BillingAddress.FirstName,
                     order.BillingAddress.LastName,
-                    order.BillingAddress.EmailAddress,
+                    order.BillingAddress.EmailAddress!,
                     order.BillingAddress.AddressLine,
                     order.BillingAddress.Country,
                     order.BillingAddress.State,
