a00dc4602a419cd59a8ad9802b9d02b5372e397a
Saidul Islam Rajib <saidulislamrajib1997@gmail.com>
2024-12-25 01:43:11 +0600

    Use health check

--- src/Services/Ordering/Ordering.API/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -1,16 +1,20 @@
 ﻿using BuildingBlocks.Exceptions.Handler;
 using Carter;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 namespace Ordering.API
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddApiServices(this IServiceCollection services)
+        public static IServiceCollection AddApiServices(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddCarter();
 
             // Use custom exception handler is defined in `BuildingBlocks`
             services.AddExceptionHandler<CustomExceptionHandler>();
+            services.AddHealthChecks()
+                .AddSqlServer(configuration.GetConnectionString("Database")!);
 
             return services;
         }
@@ -21,6 +25,14 @@ namespace Ordering.API
             app.MapCarter();
 
             app.UseExceptionHandler(options => { });
+
+            // https://github.com/Xabaril/AspNetCore.Diagnostics.HealthChecks?tab=readme-ov-file
+            app.UseHealthChecks("/health",
+                new HealthCheckOptions
+                {
+                    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+                }
+                );
 
             return app;
         }
