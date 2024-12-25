790f51a7e7dbe5f2f8dd1105306e580a675c1eff
Saidul Islam Rajib <saidulislamrajib1997@gmail.com>
2024-12-25 22:14:15 +0600

    Add package reference, and register masstransit

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // ............ ADD SERVICES TO THE DI CONTAINER ............
 builder.Services
-    .AddApplicationServices()
+    .AddApplicationServices(builder.Configuration)
     .AddInfrastructureServices(builder.Configuration)
     .AddApiServices(builder.Configuration);
 
