4af1cc12994606da8d839c5600314b2922c0a5b1
Saidul Islam Rajib <saidulislamrajib1997@gmail.com>
2024-12-27 15:15:02 +0600

    Register reverse proxy

--- src/ApiGateways/ApiGateway/Program.cs
+++ src/ApiGateways/ApiGateway/Program.cs
@@ -2,9 +2,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 // ADD SERVICES TO THE CONTAINER
 
+// Add the reverse proxy capability to the server
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
 var app = builder.Build();
 
 // CONFIGURE HTTP REQUEST PIPELINE
+
+// Register the reverse proxy routes
+app.MapReverseProxy();
 
 app.MapGet("/", () => "Hello World!");
 
