11370baee0b08ad80d3da475473ff45f84442aa9
Saidul Islam Rajib <saidulislamrajib1997@gmail.com>
2024-12-27 16:38:03 +0600

    Add rate limiting functionality

--- src/ApiGateways/ApiGateway/Program.cs
+++ src/ApiGateways/ApiGateway/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // ADD SERVICES TO THE CONTAINER
@@ -6,11 +8,20 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+builder.Services.AddRateLimiter(options =>
+{
+    options.AddFixedWindowLimiter("customPolicy", opt =>
+    {
+        opt.PermitLimit = 5;
+        opt.Window = TimeSpan.FromSeconds(12);
+    });
+});
+
 var app = builder.Build();
 
 // CONFIGURE HTTP REQUEST PIPELINE
 
-// Register the reverse proxy routes
-app.MapReverseProxy();
+app.UseRateLimiter(); // Add rate limiter middleware
+app.MapReverseProxy(); // Add reverse proxy middleware
 
 app.Run();
