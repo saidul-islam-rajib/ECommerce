15c7e51e530d41ace0190ba34856a83a13dc568d
Saidul Islam Rajib <saidulislamrajib1997@gmail.com>
2024-12-28 23:19:29 +0600

    Using refit in `Icatalogservice`

--- src/WebApps/Shopping.Web/Services/ICatalogService.cs
+++ src/WebApps/Shopping.Web/Services/ICatalogService.cs
@@ -2,7 +2,12 @@
 
 public interface ICatalogService
 {
+    [Get("catalog-service/products?pageNumber={pageNumber}&pageSize={pageSize}")]
     Task<GetProductsResponse> GetProducts(int? PageNumber = 1, int? PageSize = 10);
+
+    [Get("catalog-service/products/{id}")]
     Task<GetProductByIdResponse> GetProduct(Guid id);
+
+    [Get("catalog-service/products/category/{category}")]
     Task<GetProductByCateogryResponse> GetProductByCategory(string category);
 }
