5e71d932bc114ff447fffc1fd94171d23f3ae2ca
Saidul Islam Rajib <saidulislamrajib1997@gmail.com>
2024-12-28 23:05:57 +0600

    develop Icatalog interface

---
+++ src/WebApps/Shopping.Web/Services/ICatalogService.cs
@@ -0,0 +1,8 @@
+﻿namespace Shopping.Web.Services;
+
+public interface ICatalogService
+{
+    Task<GetProductsResponse> GetProducts(int? PageNumber = 1, int? PageSize = 10);
+    Task<GetProductByIdResponse> GetProduct(Guid id);
+    Task<GetProductByCateogryResponse> GetProductByCategory(string category);
+}
