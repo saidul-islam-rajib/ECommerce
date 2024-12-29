bfb0e0c2dae25d65cc89e825ba49065773d4a8a2
Saidul Islam Rajib <saidulislamrajib1997@gmail.com>
2024-12-29 16:36:04 +0600

    Add product detail page

--- src/WebApps/Shopping.Web/Pages/ProductDetail.cshtml.cs
+++ src/WebApps/Shopping.Web/Pages/ProductDetail.cshtml.cs
@@ -1,9 +1,41 @@
 namespace Shopping.Web.Pages
 {
-    public class ProductDetailModel : PageModel
+    public class ProductDetailModel(ICatalogService catalogService, IBasketService basketService, ILogger<ProductDetailModel> logger)
+        : PageModel
     {
-        public void OnGet()
+        public ProductModel Product { get; set; } = default!;
+
+        [BindProperty]
+        public string Color { get; set; } = default!;
+
+        [BindProperty]
+        public int Quantity { get; set; } = default!;
+        public async Task<IActionResult> OnGetAsync(Guid productId)
         {
+            var response = await catalogService.GetProduct(productId);
+            Product = response.Product;
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAddToCartAsync(Guid productId)
+        {
+            logger.LogInformation("Addd to cart button clicked.");
+            var productResponse = await catalogService.GetProduct(productId);
+
+            var basket = await basketService.LoadUserBasket();
+            basket.Items.Add(new ShoppingCartItemModel
+            {
+                ProductId = productId,
+                ProductName = productResponse.Product.Name,
+                Price = productResponse.Product.Price,
+                Quantity = 1,
+                Color = "Blace"
+            });
+
+            await basketService.StoreBasket(new StoreBasketRequest(basket));
+
+            return RedirectToPage("Cart");
         }
     }
 }
